114ffeb9090dc9d1784199c0e92ea333abecc11c
Yijin <49178058+o0heama0o@users.noreply.github.com>
2022-01-17 21:27:00 +0900

    .

--- Learn_Cpp16.cpp
+++ Learn_Cpp16.cpp
@@ -1,6 +1,15 @@
+/*content
+- generic programming (일반화 프로그래밍)
+    c++의 특징이다. 일반화 프로그래밍을 지원하는 기능이 바로 template이다. 
+
+- template
+    
+*/
+
+
 #include <iostream>
 using namespace std;
 
 int main(){
-    
+
 }
