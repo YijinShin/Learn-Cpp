52b52055a444b57d2108dd7903edb46f85fad901
Yijin <49178058+o0heama0o@users.noreply.github.com>
2022-02-16 13:53:25 +0900

    Baekjoon2751_2.cpp 수정
    정답처리 된 후,
    시간을 줄이기 위해 여러가지 시도를 해봄.
    std::ios_base::sync_with_stdio(false);
    에 대해 알게됨.

--- Baekjoon2751_2.cpp
+++ Baekjoon2751_2.cpp
@@ -1,42 +1,15 @@
 #include <iostream>
-using namespace std;
+//using namespace std;
 #define MAXSIZE 1000000
 
-void MergeSort(int* arr, int start, int end);
-void Merge(int* arr, int start, int mid, int end);
+//void MergeSort(int* arr, int start, int end);
+//void Merge(int* arr, int start, int mid, int end);
+//void MergeSort(int arr[], int start, int end);
+//void Merge(int arr[], int start, int mid, int end);
+int sorted[1000000];
 
-int sorted[MAXSIZE];
-
-int main(void){
-    int arrSize;
-    int* arr;
-
-    cin >> arrSize;
-    arr = new int(arrSize);
-
-    for(int i=0;i<arrSize;i++){
-        cin >> arr[i];
-    }
-    MergeSort(arr, 0, arrSize-1);
-    for(int i=0;i<arrSize;i++){
-        cout << arr[i]<<endl;
-    }
-
-    delete[] arr;
-    return 0;
-}
-
-void MergeSort(int* arr, int start, int end){
-    if(start >= end) return;
-    int mid = (start+end)/2;
-
-    MergeSort(arr, start, mid);
-    MergeSort(arr, mid+1, end);
-    Merge(arr, start, mid, end);
-}   
-
-void Merge(int* arr, int start, int mid, int end){
-    if(start >= end) return;
+//void Merge(int* arr, int start, int mid, int end){
+void Merge(int arr[], int start, int mid, int end){
     int l = start;
     int r = mid+1;
     int index = start;
@@ -51,6 +24,47 @@ void Merge(int* arr, int start, int mid, int end){
         for(int i=l; i<=mid; i++) sorted[index++] = arr[i];
     }
 
-    for(int i=start;i<=end;i++) arr[i] = sorted[i];
-    return;
+    for(int i=start;i<=end;i++) arr[i] = sorted[i];    
 }
+
+//void MergeSort(int* arr, int start, int end){
+void MergeSort(int arr[], int start, int end){
+    if(start >= end) return;
+    
+    int mid = (start+end)/2;
+    MergeSort(arr, start, mid);
+    MergeSort(arr, mid+1, end);
+    Merge(arr, start, mid, end);
+}   
+
+int main(void){
+    std::ios_base::sync_with_stdio(false); // 알고리즘 실행속도를 높이기 위해 쓰는 구문
+    cin.tie(null); // https://jaimemin.tistory.com/1521
+    int arrSize;
+    //int* arr;
+
+    std::cin >> arrSize;
+    //arr = new int[arrSize];
+    int arr[arrSize];
+
+    for(int i=0;i<arrSize;i++){
+        std::cin >> arr[i];
+    }
+
+    MergeSort(arr, 0, arrSize-1);
+
+    //for(int i=0;i<arrSize;i++){
+    for(auto i: arr){
+        std::cout << i<<'\n';
+    }
+
+    //delete[] arr;
+    return 0;
+}
+
+// endl 대신 \n 사용, merge에 조건문없앰 : 720ms
+// arr 동적할당 안하고 그냥 배열로 사용 : 712ms
+// namespace 사용안하고 그냥 배열로 사용 : 716ms
+// 함수 선언 위치 바꾸기 : 720ms
+// 결과 출력에 for(auto i: arr){} 사용 : 700ms
+// std::ios_base::sync_with_stdio(false); 사용 : 336ms 이거다!! 
