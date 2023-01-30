d7fdc724a46a2c7d51053282301f3b6e51f0dbf2
Yijin Shin <49178058+YijinShin@users.noreply.github.com>
2023-01-30 15:42:57 +0900

    hw20230130_text rpg

--- CppStudy_2023/CppStudy_2023/study20230130.cpp
+++ CppStudy_2023/CppStudy_2023/study20230130.cpp
@@ -2,6 +2,23 @@
 
 using namespace std;
 
-int main() {
 
+struct tagInfo {			// 구조체 이름: tagInfo
+    char szName[20];   	 	// 구조체의 맴버 1
+    int age;			// 구조체의 맴버 2
+    float weight;		// 구조체의 맴버 3
+    float height;		// 구조체의 맴버 4
+};
+
+int main() {
+    tagInfo t;
+    t = { "he", 10,10.f,10.f };
+
+    cout << endl;
+
+    tagInfo t1 = { "Jully",20 };
+    cout << " szName: " << t1.szName << endl;
+    cout << " age: " << t1.age << endl;
+    cout << " weight: " << t1.weight << endl;
+    cout << " height: " << t1.height << endl;
 }
