761c14726ecc912559306069c45cf7a93fc21309
Yijin Shin <49178058+YijinShin@users.noreply.github.com>
2023-02-04 17:41:32 +0900

    PCH study

---
+++ CppStudy_2023_vs2015/CppStudy_2023_vs2015/TestHeader_1.cpp
@@ -0,0 +1,6 @@
+#include "stdafx.h"
+#include "TestHeader_1.h"
+
+void TestHeaderClass1::Display() {
+	cout << "TestHeaderClass 1" << endl;
+}
---
+++ CppStudy_2023_vs2015/CppStudy_2023_vs2015/TestHeader_1.h
@@ -0,0 +1,7 @@
+#pragma once
+//#include "stdafx.h" // 헤더는 안해도그만. stdafx.h안에 있는 라이브러리나 헤더관련 뭔가 안쓰면 include안해도됨
+
+class TestHeaderClass1 {
+public:
+	void Display();
+};
