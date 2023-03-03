3b50ea5a23d07cd58e0072fadb899baf91eb9b22
Yijin Shin <49178058+YijinShin@users.noreply.github.com>
2023-03-03 10:07:37 +0900

    study c++11, 예외처리

--- LearnCpp2023_phase2/LearnCpp2023_phase2/study_CPP11.cpp
+++ LearnCpp2023_phase2/LearnCpp2023_phase2/study_CPP11.cpp
@@ -3,16 +3,27 @@
 - auto 키워드
 - 범위 기반 for문 
 - 유니폼 초기화
+	유니폼 초기화 ({} 를 이용한 생성자 호출) 를 통해서 인자 없는 생성자가 함수의 정의로 오해되는 일을 막을 수 있으며 
+	initializer_list 를 만들어 전달할 수 있습니다. 
+	initializer_list 를 통해서 객체를 간단하게 생성할 수 있습니다.
 
 */
 
 #include "stdafx.h"
 #include <vector>
+#include <list>
 using namespace std;
 
 class CTest {
 public:
 	CTest() { cout << "CTest생성자" << endl; };
+};
+
+class CTest2 {
+public:
+	CTest2() { cout << "인자가 없는 생성자" << endl; }
+	CTest2(int _i, double _d) { cout << "int, double을 받는 생성자 " << endl; }
+	CTest2(std::initializer_list<int> _list) { cout << "list<int>를 받는 생성자 " << endl; }
 };
 
 int main() {
@@ -39,7 +50,15 @@ int main() {
 
 	/* 유니폼 초기화 */
 	{
-		// {}로 초기화 하는 방법
+		// {}로 인자 없는 생성자 호출하기
 		CTest ct{};
+
+		// {}가 컴파일되는 경우의수 
+		CTest2 t1{};
+		CTest2 t2(10, 10.4);
+		//CTest2 t3{ 10, 10.4 };	// initializer_list를 인자로 받는 생성자가 있고, initializer_list의 타입이 int라서 초기화 리스트로 인식됨.
+									// 근데 {}는 부동소수점타입 <-> 정수 타입간의 암시적 형변환을 막기 때문에 에러가 남. 
+									// (int, double) 생성자를 부르려면 1. initializer_list의 타입을 아예 string이렇게 바꿔버리거나(암시적 변환이 아예 안되는 타입으로) 
+									//								  2. initializer_list 를 인자로 가지는 생성자를 없애면됨. 
 	}
 }
---
+++ LearnCpp2023_phase2/LearnCpp2023_phase2/study_STL_altorithm.cpp
@@ -0,0 +1,16 @@
+/* [ STL algorithm ÇÔ¼ö ]
+
+- sort()
+- count_t
+- for which
+
+*/
+
+#include "stdafx.h"
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+}
