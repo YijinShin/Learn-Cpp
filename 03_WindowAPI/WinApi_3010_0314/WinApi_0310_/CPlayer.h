8cdc3f8db36bffa73c5ee197fc740bf0f983f992
Yijin Shin <49178058+YijinShin@users.noreply.github.com>
2023-03-12 02:52:00 +0900

    window api study

---
+++ 03_WindowAPI/WinApi_3010_0314/WinApi_0310_/CPlayer.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "CObj.h"
+
+
+class CPlayer: public CObj
+{
+private:
+	list<CObj*>* m_pBulletList;
+public:
+	CPlayer();
+	virtual ~CPlayer();
+
+	virtual void	Initialize()	override;
+	virtual	int		Update()		override;
+	virtual int		Late_Update()	override;
+	virtual void	Render(HDC hDC)		override;
+	virtual void	Release()		override;
+
+	void Key_Input();
+	
+	void Set_BulletList(list<CObj*>* _pBulletList) { m_pBulletList = _pBulletList; }
+
+};
+
--- WindowAPI/WindowProject02/WinApi_0310_/Enum.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-enum DIR {DIR_UP = 1 , DIR_DOWN, DIR_LEFT, DIR_RIGHT, DIR_LU, DIR_RU};
